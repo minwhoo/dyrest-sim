6c97361fb0f0843780de3f5483a49bc6cf8160e0
Minwoo Lee <minwoo.lee@yonsei.ac.kr>
2017-11-30 12:29:55 +0900

    Implement random availability init

--- main.go
+++ main.go
@@ -6,15 +6,22 @@ import (
 
 func initializeNodes(pool *[]*node, file *segfile) {
 	for i := 0; i < 5; i++ {
-		newNode(pool, file, 100, 10, 10, 10)
+		var ratio float64 = 0
+		if (i == 0) {
+			ratio = 1
+		}
+		newNode(pool, file, 100, 10, 10, 10, ratio)
 	}
 }
 
 func startSimulation() {
 	fmt.Println("Starting simulation...")
+
 	segfile := newSegfile(12*MB, 10, 512*KB)
 	nodePool := []*node{}
+
 	initializeNodes(&nodePool, &segfile)
+
 	fmt.Println("Simulation done!")
 }
 
--- node.go
+++ node.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"time"
+	"math/rand"
+)
+
 var nodeIdx = 0
 
 type node struct {
@@ -15,7 +20,19 @@ type node struct {
 	segfile *segfile
 }
 
-func newNode(pool *[]*node, file *segfile, dlBandwidth float64, ulBandwidth float64, maxDownloadLinks int, maxUploadLinks int) {
+func getRandomAvailability(ratio float64, numChunks int) []bool {
+	availability := make([]bool,numChunks)
+	rand.Seed(time.Now().UnixNano())
+
+	for _, idx := range rand.Perm(numChunks)[:int(ratio*float64(numChunks))] {
+		availability[idx] = true
+	}
+
+	return availability
+}
+
+func newNode(pool *[]*node, file *segfile, dlBandwidth float64, ulBandwidth float64, maxDownloadLinks int, maxUploadLinks int, availabilityRatio float64) {
+
 	n := node{
 		id: 					nodeIdx,
 		downloadBandwidth:		dlBandwidth,
@@ -23,7 +40,7 @@ func newNode(pool *[]*node, file *segfile, dlBandwidth float64, ulBandwidth floa
 		maxDownloadLinks:		maxDownloadLinks,
 		maxUploadLinks:			maxUploadLinks,
 		connectedNodes: 		[]*node{},
-		dataChunkAvailability: 	make([]bool,file.numDataChunks),
+		dataChunkAvailability: 	getRandomAvailability(availabilityRatio,file.numDataChunks),
 		parityChunkAvailability:[][]bool{},
 		pool:					pool,
 		segfile: 				file,
